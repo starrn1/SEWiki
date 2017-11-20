372df2f0f4c198a74eddc783ff9781330e1e649a
Nicholas Starr <>
2017-11-19 20:42:48 -0500

    Clear edit user text field on refresh.

--- Riki-deploy-ver2/config.py
+++ Riki-deploy-ver2/config.py
@@ -1,11 +1,11 @@
 # encoding: utf-8
 
 SECRET_KEY='a unique and long key'
-TITLE='Riki' 
+TITLE='Riki'
 HISTORY_SHOW_MAX=30
 PIC_BASE = '/static/content/'
-CONTENT_DIR = '/Users/matev/PyCharmProjects/SEWiki/Riki-deploy-ver2/content'
-USER_DIR = '/Users/matev/PyCharmProjects/SEWiki/Riki-deploy-ver2/user'
-BROWSER_PATH = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+CONTENT_DIR = '/Users/Nick/Documents/Github/SEWiki/Riki-deploy-ver2/content'
+USER_DIR = '/Users/Nick/Documents/Github/SEWiki/Riki-deploy-ver2/user'
+BROWSER_PATH = 'open -a /Applications/Google\ Chrome.app %s'
 NUMBER_OF_HISTORY = 5
 PRIVATE = True
--- Riki-deploy-ver2/wiki/web/routes.py
+++ Riki-deploy-ver2/wiki/web/routes.py
@@ -22,17 +22,13 @@ from wiki.web.forms import URLForm
 from wiki.web import current_wiki
 from wiki.web import current_users
 from wiki.web.user import protect
-<<<<<<< HEAD
 from wiki.web.forms import CreateUserForm
 from wiki.web.user import UserManager
 import config
-=======
-
 import pypandoc
 import webbrowser
 import config
 import os
->>>>>>> 59f54a6e298c30031611262a38e9239f0d3be6eb
 
 bp = Blueprint('wiki', __name__)
 
@@ -206,7 +202,7 @@ def admin_page():
                 current_users.delete_user(form.user_edit.data)
         else:
             flash('User not found.', 'error')
-
+    form.user_edit.data = ''
     users = {}
     x = current_users.read()    #returns a dict object with unicode values
     for key, values in x.items():
