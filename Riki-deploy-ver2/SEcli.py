9dc9dfed1c5c6925ef03275a2503ba645e1166eb
Mate Virag <viragm1@nku.edu>
2017-12-03 18:24:06 -0500

    Fixed a bug where the file doesn't exist for PDF conversion

--- Riki-deploy-ver2/SEcli.py
+++ Riki-deploy-ver2/SEcli.py
@@ -7,13 +7,17 @@ import config
 def convert(file):
     output = file[:-3]
     file = os.path.join(config.CONTENT_DIR, file)
-    pypandoc.convert_file(file, 'pdf', outputfile="content/pdf/" + output + ".pdf")
-    abspath = os.path.abspath(config.CONTENT_DIR + "/pdf/"  + output + ".pdf")
-    webbrowser.get(config.BROWSER_PATH).open_new_tab(abspath)
+    if os.path.isfile(file):
+        pypandoc.convert_file(file, 'pdf', outputfile="content/pdf/" + output + ".pdf")
+        abspath = os.path.abspath(config.CONTENT_DIR + "/pdf/"  + output + ".pdf")
+        webbrowser.get(config.BROWSER_PATH).open_new_tab(abspath)
+        return "PDF conversion was successful."
+    else:
+        return "The given file does not exist."
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--pdf', help='PDF Conversion')
 args = parser.parse_args()
 if args.pdf is not None:
-    convert(args.pdf)
+    print(convert(args.pdf))
 
