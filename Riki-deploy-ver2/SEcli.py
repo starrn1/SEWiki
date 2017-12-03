87a1eb1795aa8e1524e6182aac1f172947853f1e
Nicholas Starr <>
2017-12-03 18:10:55 -0500

    Began CLI implementation

---
+++ Riki-deploy-ver2/SEcli.py
@@ -0,0 +1,19 @@
+import argparse
+import pypandoc
+import os
+import webbrowser
+import config
+
+def convert(file):
+    output = file[:-3]
+    file = os.path.join(config.CONTENT_DIR, file)
+    pypandoc.convert_file(file, 'pdf', outputfile="content/pdf/" + output + ".pdf")
+    abspath = os.path.abspath(config.CONTENT_DIR + "/pdf/"  + output + ".pdf")
+    webbrowser.open_new(abspath)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--pdf', help='PDF Conversion')
+args = parser.parse_args()
+if args.pdf is not None:
+    convert(args.pdf)
+
