066d3ad44c860a25017a09e3e3cfe7c06ddc0033
Mate Virag <viragm1@nku.edu>
2017-12-03 18:46:25 -0500

    Implemented smart delete for the command-line interface

--- Riki-deploy-ver2/SEcli.py
+++ Riki-deploy-ver2/SEcli.py
@@ -15,9 +15,38 @@ def convert(file):
     else:
         return "The given file does not exist."
 
+def smartDelete(file):
+    fileSplit = file.split('.')
+    if (fileSplit[1] == "pdf"):
+        file = os.path.join(config.CONTENT_DIR + "/pdf/", file)
+        if os.path.isfile(file):
+            os.remove(file)
+            return "The PDF file was successfully deleted."
+        else:
+            return "The given PDF file does not exist."
+    elif (fileSplit[1] == "md"):
+        file = os.path.join(config.CONTENT_DIR, file)
+        if os.path.isfile(file):
+            pdfFile = os.path.join(config.CONTENT_DIR + "/pdf/", fileSplit[0] + ".pdf")
+            if (os.path.isfile(pdfFile)):
+                os.remove(pdfFile)
+                os.remove(file)
+                return "Both the markdown and its corresponding PDF file were deleted."
+            else:
+                os.remove(file)
+                return "The markdown file was successfully deleted."
+        else:
+            return "The given markdown file does not exist."
+    else:
+        return "Please enter an existing PDF or markdown file name."
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--pdf', help='PDF Conversion')
+parser.add_argument('-d', '--delete', help='File deletion')
 args = parser.parse_args()
 if args.pdf is not None:
     print(convert(args.pdf))
+
+if args.delete is not None:
+    print(smartDelete(args.delete))
 
